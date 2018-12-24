01f0d22b6e2457516193567e93f34495b485b239
Rudy <44776187+rudydekeerschieter@users.noreply.github.com>
2018-12-24 17:51:07 +0100

    Delete PrinterTaskCopy.java

--- ISP/ISPsolution/PrinterTaskCopy.java
+++
@@ -1,9 +0,0 @@
-/**
- * Only some printers can also copy, so this function gets an interface by itself.
- */
-
-package be.ucll.ooo.solid;
-
-public interface PrinterTaskCopy {
-    public boolean photoCopyContent(String content);
-}
